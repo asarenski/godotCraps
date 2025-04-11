5615ea8ba4b643bdca35b556bc046600ad7a7952
Adam Sarenski <asarenski@gmail.com>
2025-04-11 09:03:16 -0400

    init

---
+++ start.gd
@@ -0,0 +1,12 @@
+extends CanvasLayer
+
+# Notifies `Main` node that the button has been pressed
+signal start_game
+
+func _on_start_button_pressed():
+	$StartButton.hide()
+	start_game.emit()
+
+func show_message(text):
+	$Message.text = text
+	$Message.show()
